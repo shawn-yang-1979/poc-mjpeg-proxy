3ae7d2ca39df6bc659755e398735a51e5343f1e8
SHAWN.SH.YANG <SHAWN.SH.YANG@TWTP1NB1024.delta.corp>
2019-02-27 12:46:49 +0800

    Project init

---
+++ poc-mjpeg-proxy/src/main/java/idv/shawnyang/mjpegproxy/PocMjpegProxyApplication.java
@@ -0,0 +1,13 @@
+package idv.shawnyang.mjpegproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMjpegProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMjpegProxyApplication.class, args);
+	}
+
+}
---
+++ poc-mjpeg-proxy/src/test/java/idv/shawnyang/mjpegproxy/PocMjpegProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package idv.shawnyang.mjpegproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocMjpegProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
