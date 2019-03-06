82083beb3056a152ad1175b2e02ca30cdda539fe
SHAWN.SH.YANG <SHAWN.SH.YANG@TWTP1NB1024.delta.corp>
2019-03-06 16:24:02 +0800

    fix java doc incorrect author

--- poc-mjpeg-proxy/src/main/java/idv/shawnyang/mjpegproxy/MjpegProxyProperties.java
+++ poc-mjpeg-proxy/src/main/java/idv/shawnyang/mjpegproxy/MjpegProxyProperties.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Component;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * @author SCOTT.SU
- * @Date 2018-12-04 上午10:30:37
- */
 @Getter
 @Setter
 @Component
